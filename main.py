2e7ef17b4999888d8a097845a73ba5ea6eab54d7
Francisco <franciscofonsecarickk@gmail.com>
2025-10-06 19:06:16 -0300

    rota de estatisticas

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from flask import Flask, jsonify
+import datetime
 
 app = Flask(__name__)
 
-
+class Estatisticas:
+    def __init__(self, nome, abates, mortes, assistencias, dano, data, dinheiro): #construtor
+        self.nome = nome
+        self.abates = abates
+        self.mortes = mortes
+        self.assistencias = assistencias
+        self.dano = dano
+        self.data = data
+        self.dinheiro = dinheiro
+    
+    def calcular_kd(self): #definição de função
+        return self.abates/self.mortes
+    
 @app.route('/')
 def index():
 	"""Rota principal que retorna um JSON simples"""
@@ -12,6 +25,19 @@ def index():
 		"endpoints": ["/"],
 		"versao": "1.0.0"
 	})
+
+@app.route('/estatisticas')
+def ola():
+    estatistica = Estatisticas("Chico", 21, 12, 10, 2300, datetime.date(2025,10,1), 8000 )
+    return jsonify({
+          "nome": estatistica.nome,
+          "abates": estatistica.abates,
+          "mortes": estatistica.mortes,
+          "assistencias": estatistica.assistencias,
+          "dano": estatistica.dano,
+          "data": estatistica.data,
+          "dinheiro": estatistica.dinheiro
+    })
 
 
 if __name__ == '__main__':
